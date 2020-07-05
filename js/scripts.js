d65d8670f21659e56e0ccd6282cdc7e8fa927653
ovi100 <ovi0167@hotmail.com>
2020-07-05 23:05:32 +0600

    project file uploaded

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$(".search-input").focus(function (){
+        $(".search-suggestions").fadeIn("slow");
+    });
+
+    $(".search-input").blur(function (){
+        $(".search-suggestions").fadeOut("slow");
+    });
+
+  	var menuHeight = $(".menu1").height();
+    $(".sidenav").css("top",menuHeight+'px');
+    $(".sidenav-overlay").css("top",menuHeight+'px');
+
+    $("#sidebar-toggle").click(function(event){
+    	event.preventDefault();
+    	$("#sidenav").toggleClass("show-sidenav");
+    });
+    $("#sidenav-overlay").click(function(event){
+    	event.preventDefault();
+    	$("#sidenav").removeClass("show-sidenav");
+    });
+
+    $('.cat-list .collapse').on('shown.bs.collapse', function(){
+        $(this).parent().find(".icofont-plus").removeClass("icofont-plus").addClass("icofont-minus");
+    }).on('hidden.bs.collapse', function(){
+        $(this).parent().find(".icofont-minus").removeClass("icofont-minus").addClass("icofont-plus");
+    });
+
+});
